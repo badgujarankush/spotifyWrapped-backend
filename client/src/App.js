e8fd4eb17e1e3d1a12a0df0aebbd6a93f5be4620
badgujarankush <ankush.badgujar58@gmail.com>
2022-07-31 12:13:19 +0530

    added basics for refresh token and template of react frontend

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <a
+          className="App-link"
+          href="http://localhost:8888/login"
+        >
+          Login To Spotify
+        </a>
+    </div>
+  );
+}
+
+export default App;
--- server.js
+++ server.js
@@ -1,13 +1,124 @@
 require('dotenv').config();
+const { query } = require('express');
 const express = require('express');
+const querystring = require('querystring');
+const axios  = require('axios');
 const app = express();
 const port  = 8888;
-console.log(process.env.CLIENT_ID);
-app.get('/',(req,res)=>{
-    res.send("Hello user");
-    
-})
 
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+
+app.get('/',(req,res)=>{
+    res.send('Hello');
+    
+});
+
+
+
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+ const generateRandomString = length => {
+    let text = '';
+    const possible =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  };
+  
+  const stateKey = 'spotify_auth_state';
+
+
+
+app.get('/login',(req,res)=>{
+    const state = generateRandomString(16);
+    res.cookie(stateKey,state);
+
+    const scopes = [
+        'user-read-email',
+        'user-read-private',
+        'playlist-read-collaborative',
+        'playlist-read-private',
+        'user-library-read',
+        'user-top-read',    
+        'user-read-recently-played',
+        'user-follow-read',
+    ].join(' ');
+
+    const queryParams = querystring.stringify({
+        client_id : CLIENT_ID,
+        response_type : 'code',
+        redirect_uri :REDIRECT_URI,
+        state: state
+    })
+    res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
+})
+app.get('/callback',(req,res)=>{
+    const code = req.query.code || null;
+
+    axios({
+      method: 'post',
+      url: 'https://accounts.spotify.com/api/token',
+      data: querystring.stringify({
+        grant_type: 'authorization_code',
+        code: code,
+        redirect_uri: REDIRECT_URI
+      }),
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
+      },
+    })
+      .then(response => {
+        if(response.status === 200){
+          const {refresh_token } = response.data;
+
+        axios.get(`http://localhost:8888/refresh_token?refresh_token=${refresh_token}`)
+          .then(response => {
+            res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
+          })
+          .catch(error => {
+            res.send(error);
+          });
+        }else{
+          res.send(response);
+        }
+        
+      })
+      .catch(error => {
+        res.send(error);
+      });
+});
+
+app.get('/refresh_token', (req, res) => {
+    const { refresh_token } = req.query;
+  
+    axios({
+      method: 'post',
+      url: 'https://accounts.spotify.com/api/token',
+      data: querystring.stringify({
+        grant_type: 'refresh_token',
+        refresh_token: refresh_token
+      }),
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
+      },
+    })
+      .then(response => {
+        res.send(response.data);
+      })
+      .catch(error => {
+        res.send(error);
+      });
+  });
+  
 app.listen(port,()=>{
     console.log(`App listening at port ${port}`)
 })
